4a236cba2cd92388ce32f4c32eb157012bf9803c
黄维 <2315044617@qq.com>
2019-12-16 20:40:06 +0800

    test commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld{
     public static void main(String[] args){
 	System.out println("HelloWorld");
 	System.out println("11");
-	System.out println("HelloWorld");
-	System.out println("22");
+	System.out println("hhh");
 }
 }
