8976e47367354bf6882e3f6a1e13ea9a74d93765
黄维 <2315044617@qq.com>
2019-12-16 18:29:22 +0800

    hello1

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args){
+	System.out println("HelloWorld");
+	System.out println("11");
+	System.out println("HelloWorld");
+	System.out println("22");
+}
+}
